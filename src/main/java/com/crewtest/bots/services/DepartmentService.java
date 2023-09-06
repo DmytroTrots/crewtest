039a5ad0c1ee918fcb74c097c0d834f2df65994b
DmytroTrots <dtrots27@gmail.com>
2023-09-06 15:59:07 +0300

    BotsTest#7: changes to avoid exceptions during execution.

--- src/main/java/com/crewtest/bots/services/DepartmentService.java
+++ src/main/java/com/crewtest/bots/services/DepartmentService.java
@@ -1,10 +1,14 @@
 package com.crewtest.bots.services;
 
 import com.crewtest.bots.entities.DepartmentEntity;
+import com.crewtest.bots.entities.LectorEntity;
 import com.crewtest.bots.repositories.DepartmentRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -21,20 +25,50 @@ public class DepartmentService {
     }
 
     public String findHeadNameOfDepartment(String departmentName) {
-        return findDepartmentByName(departmentName).getHeadOfDepartmentName();
+        DepartmentEntity department = findDepartmentByName(departmentName);
+        if (department != null){
+            return department.getHeadOfDepartmentName();
+        }
+        return null;
     }
 
+    @Transactional(propagation=Propagation.REQUIRED, readOnly=true, noRollbackFor=Exception.class)
     public Map<String, Long> showNumberOfLectorsForEachDegree(String departmentName) {
-        return findDepartmentByName(departmentName).getLectors().stream()
-                .collect(Collectors.groupingBy(x -> x.getDegreeId().getDegreeName(), Collectors.counting()));
+        DepartmentEntity department = findDepartmentByName(departmentName);
+        if (department == null){
+            return null;
+        }
+        Set<LectorEntity> lectorSet = department.getLectors();
+        if (lectorSet == null || lectorSet.isEmpty()) {
+            return null;
+        }
+        return lectorSet.stream().collect(Collectors.groupingBy(x -> x.getDegreeId().getDegreeName(), Collectors.counting()));
     }
 
+    @Transactional(propagation=Propagation.REQUIRED, readOnly=true, noRollbackFor=Exception.class)
     public double countAverageSalaryForDepartment(String departmentName) {
-        return findDepartmentByName(departmentName).getLectors().stream()
+        DepartmentEntity department = findDepartmentByName(departmentName);
+        if (department == null){
+            return 0;
+        }
+        Set<LectorEntity> lectorSet = department.getLectors();
+        if (lectorSet == null || lectorSet.isEmpty()) {
+            return 0;
+        }
+        return lectorSet.stream()
                 .mapToDouble(x -> x.getSalary().doubleValue()).average().orElse(0);
     }
 
+    @Transactional(propagation=Propagation.REQUIRED, readOnly=true, noRollbackFor=Exception.class)
     public int countNumberOfLectorsForDepartment(String departmentName) {
-        return findDepartmentByName(departmentName).getLectors().size();
+        DepartmentEntity department = findDepartmentByName(departmentName);
+        if (department == null){
+            return 0;
+        }
+        Set<LectorEntity> lectorSet = department.getLectors();
+        if (lectorSet == null || lectorSet.isEmpty()) {
+            return 0;
+        }
+        return lectorSet.size();
     }
 }
--- src/main/java/com/crewtest/bots/utils/InputReader.java
+++ src/main/java/com/crewtest/bots/utils/InputReader.java
@@ -10,6 +10,12 @@ import java.io.InputStreamReader;
 @Component
 public class InputReader implements CommandLineRunner {
 
+    private final RequestDecider requestDecider;
+
+    public InputReader(RequestDecider requestDecider) {
+        this.requestDecider = requestDecider;
+    }
+
     @Override
     public void run(String... args) throws IOException {
         System.out.println("Введіть Ваш запит!");
@@ -17,6 +23,7 @@ public class InputReader implements CommandLineRunner {
         String line = "";
         while (!line.equals("0")) {
             line = buffReader.readLine();
+            requestDecider.getNeededRequest(line);
         }
 
         System.exit(0);
